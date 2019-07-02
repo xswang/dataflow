49fd9eff49ff5251b199ba6207b7edd53224c263
xswang <2012wxs@gmail.com>
2019-07-02 16:12:38 +0800

    update: ps-lite

--- cmake/third_party.cmake
+++ cmake/third_party.cmake
@@ -10,8 +10,6 @@ include(protobuf)
 include(googletest)
 include(glog)
 include(gflags)
-# include(grpc)
-# include(zeromq)
 include(eigen)
 
 set(dataflow_third_party_libs
@@ -21,8 +19,6 @@ set(dataflow_third_party_libs
     ${GFLAGS_STATIC_LIBRARIES}
     ${GOOGLETEST_STATIC_LIBRARIES}
     ${PROTOBUF_STATIC_LIBRARIES}
-    # ${GRPC_STATIC_LIBRARIES}
-    # ${ZEROMQ_STATIC_LIBRARIES}
 )
 
 set(dataflow_third_party_dependencies
@@ -37,21 +33,14 @@ set(dataflow_third_party_dependencies
   protobuf_copy_headers_to_destination
   protobuf_copy_libs_to_destination
   protobuf_copy_binary_to_destination
-  # grpc_copy_headers_to_destination
-  # grpc_copy_libs_to_destination
-  # grpc_copy_binary_to_destination
-  # zeromq_copy_headers_to_destination
-  # zeromq_copy_libs_to_destination
   eigen_copy_headers_to_destination
 )
 
 include_directories(
-    # ${ZLIB_INCLUDE_DIR}
+    ${ZLIB_INCLUDE_DIR}
     ${GFLAGS_INCLUDE_DIR}
     ${GLOG_INCLUDE_DIR}
     ${GOOGLETEST_INCLUDE_DIR}
     ${PROTOBUF_INCLUDE_DIR}
-    # ${GRPC_INCLUDE_DIR}
-    # ${ZEROMQ_INCLUDE_DIR}
     ${EIGEN_INCLUDE_DIR}
 )
